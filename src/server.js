09a59437da5f0fa272e6f3201b77d7384f19f8cc
srdjan <srdjan@timok.com>
2014-08-19 10:38:46 -0400

    server cleanup

--- src/server.js
+++ src/server.js
@@ -6,47 +6,39 @@ var fs = require("fs");
 var fx = require('./fx.js');
 var app = require('./app.js');
 
-port = process.argv[2] || 8050;
+port = process.argv[2] || 8070;
+
+function statusCode(result) {
+  return result.hasOwnProperty('statusCode') ? result.statusCode : 200;
+}
+
+function createResponse(result, response) {
+  response.writeHeader(statusCode(result), { "Content-Type": "application/json" });
+  response.write(JSON.stringify(result));
+  response.end();
+  return response;
+}
 
 function processApi(req, response) {
   var body = '';
   if (req.method === 'POST' || req.method === 'PUT' || req.method === 'PATCH') {
-    req.on('data', function(chunk) {
-      fx.log('on.data');
-      body += chunk.toString();
-    });
-
+    req.on('data', function(chunk) { body += chunk.toString(); });
     req.on('end', function() {
-      fx.log('on.end');
       req.body = JSON.parse(body);
       var result = fx.handle(app, req);
-      var code = result.hasOwnProperty('statusCode') ? result.statusCode : 200;
-      response.writeHeader(code, {
-        "Content-Type": "application/json"
-      });
-      response.write(JSON.stringify(result));
-      response.end();
-      return response;
+      return createResponse(result, response);
     });
   }
   else {
     var result = fx.handle(app, req);
-    var code = result.hasOwnProperty('statusCode') ? result.statusCode : 200;
-    response.writeHeader(code, {
-      "Content-Type": "application/json"
-    });
-    response.write(JSON.stringify(result));
-    response.end();
-    return response;
+    return createResponse(result, response);
   }
 };
 
 function processStaticFiles(fileName, response) {
   fs.readFile(fileName, "binary", function(err, file) {
     if (err) {
-      response.writeHead(500, {
-        "Content-Type": "text/plain"
-      });
+      response.writeHead(500, { "Content-Type": "text/plain" });
       response.write(err + "\n");
       response.end();
     }
@@ -62,10 +54,8 @@ function processStaticFiles(fileName, response) {
 //-- server --
 //--
 http.createServer(function(request, response) {
-  var root = process.cwd();
   var uri = url.parse(request.url).pathname;
   uri = fx.trimLeftAndRight(uri, '/');
-
   if (uri.indexOf('api/') !== -1) {
     return processApi(request, response);
   }
@@ -79,14 +69,12 @@ http.createServer(function(request, response) {
   else {
     tokens.unshift('hal-browser');
   }
-  var fileName = path.join(root, tokens.join('/'));
 
+  var fileName = path.join(process.cwd(), tokens.join('/'));
   fs.exists(fileName, function(exists) {
     if (!exists) {
       fx.log('Not found, fileName: ' + fileName);
-      response.writeHeader(404, {
-        "Content-Type": "application/json"
-      });
+      response.writeHeader(404, { "Content-Type": "application/json" });
       response.end();
       return response;
     }
