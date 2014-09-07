515d1b1c5c9688143d6628a2b80269e3561adac5
srdjan <srdjan@timok.com>
2014-09-07 12:18:00 -0400

    reafactored

--- src/hal.js
+++ src/hal.js
@@ -5,19 +5,11 @@ var halson = require('halson');
 var fn = require('./fn.js');
 var log = console.log;
 
-function addSelfLink(hal, href) {
-  return hal.addLink('self', href);
-}
-
-function addLink(hal, rel, href, method) {
-  return hal.addLink(rel, { href: href, method: method });
-}
-
 function addLinks(halRep, ctx) {
   var links = fn.getLinks(ctx.result);
-  fn.each(function(l) {
-      addLink(halRep, l.rel, '/api/' + ctx.typeName + 's/' + fn.atob(ctx.result.id + '/' + l.rel), l.method);
-    }, links);
+  links.forEach(function(l) {
+      halRep.addLink(l.rel, { href: '/api/' + ctx.typeName + 's/' + fn.atob(ctx.result.id + '/' + l.rel), method: l.method});
+    });
   return halRep;
 }
 
@@ -28,8 +20,8 @@ function addProperties(halRep, result) {
 
 function createRoot(typeName) {
   var halRep = halson({});
-  addSelfLink(halRep, '/api/' + typeName + 's');
-  addLink(halRep, 'create', '/api/' + typeName + 's/' + fn.atob('create'), 'POST');
+  halRep.addLink('self', '/api/' + typeName + 's');
+  halRep.addLink('create', { href: '/api/' + typeName + 's/' + fn.atob('create'), method: 'POST'});
   return halRep;
 }
 
@@ -37,7 +29,7 @@ function createList(ctx) {
   var halRep = createRoot(ctx.typeName.toLowerCase());
   if (ctx.result.length > 0) {
     ctx.result.forEach(function(el, index, array) {
-        var link = addSelfLink(halson({}), '/api/' + ctx.typeName.toLowerCase() + 's/' + fn.atob(el.id));
+        var link = halson({}).addLink('self', '/api/' + ctx.typeName.toLowerCase() + 's/' + fn.atob(el.id));
         halRep.addEmbed(ctx.typeName.toLowerCase() + 's', link);
       }
     );
@@ -48,7 +40,7 @@ function createList(ctx) {
 function createOne(ctx) {
   var halRep = halson({});
   addProperties(halRep, ctx.result);
-  addSelfLink(halRep, '/api/' + ctx.typeName.toLowerCase() + 's/' + fn.atob(ctx.result.id));
+  halRep.addLink('self', '/api/' + ctx.typeName.toLowerCase() + 's/' + fn.atob(ctx.result.id));
   addLinks(halRep, ctx);
   return halRep;
 }
