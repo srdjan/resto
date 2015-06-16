e1340e714676b49f455e34b4b5675464d87b6f5d
Srdjan <srdjan@timok.com>
2015-06-16 09:54:47 -0400

    Update app.js

--- examples/apple-farm/app.js
+++ examples/apple-farm/app.js
@@ -1,22 +1,22 @@
-const pipeline      = require('../../src/pipeline');
-const authenticator = require('../../src/authn').auth;
-const authorizer    = require('../../src/authr').auth;
-const resolver      = require('../../src/resolver').resolve;
-const invoker       = require('../../src/invoker').invoke;
-const converter     = require('../../src/hal').convert;
-const http          = require('../../src/server');
+const pipeline      = require('../../src/pipeline')
+const authenticator = require('../../src/authn').auth
+const authorizer    = require('../../src/authr').auth
+const resolver      = require('../../src/resolver').resolve
+const invoker       = require('../../src/invoker').invoke
+const converter     = require('../../src/hal').convert
+const http          = require('../../src/server')
 
-const apple         = require('./resources/apple');
-const log           = console.log;
+const apple         = require('./resources/apple')
+const log           = console.log
 
-const appleResource = apple;
+const appleResource = apple
 
 const reqResp = pipeline.expose(appleResource)
                       .use(authenticator)
                       .use(resolver)
                       .use(authorizer)
                       .use(invoker)
-                      .use(converter);
+                      .use(converter)
 
-const EndPoint = http.create(reqResp);
-EndPoint.start(8070);
+const EndPoint = http.create(reqResp)
+EndPoint.start(8070)
