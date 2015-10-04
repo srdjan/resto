06eb63b552ca2731389213374e06b336a37218fc
srdjan <srdjan@timok.com>
2015-10-04 15:29:17 -0400

    hal-browser UI cleanup

--- build.bat
+++ build.bat
@@ -3,5 +3,7 @@
 set targetFolder=%~dp0examples\apple-farm\lib
 echo Target folder: %targetFolder%
 
+xcopy /S /I /Y %~dp0hal-browser %~dp0examples\apple-farm\hal-browser\
+
 babel src --out-dir %targetFolder%
 
