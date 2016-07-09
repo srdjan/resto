9c0de790b745b1c220baccb18761dc2af71f6c12
srdjan <srdjan@timok.com>
2016-07-09 09:00:07 -0400

    fixed build script

--- build.bat
+++ build.bat
@@ -3,7 +3,9 @@
 set targetFolder=%~dp0examples\apple-farm\lib
 echo Target folder: %targetFolder%
 
-xcopy /S /I /Y %~dp0hal-browser %~dp0examples\apple-farm\hal-browser\
+node src/index.js
 
-node src/server.js --out-dir %targetFolder%
+xcopy /S /I /Y %~dp0hal-browser %~dp0examples\apple-farm\hal-browser\
+xcopy /S /I /Y %~dp0src %~dp0examples\apple-farm\lib\
+
 
